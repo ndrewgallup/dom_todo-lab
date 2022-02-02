846038ddcfff36d63a5afcefb728751bf746ccf8
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-02-02 16:53:20 -0600

    cache elements with Ids

---
+++ js/app.js
@@ -0,0 +1,7 @@
+//create cached element reference for input
+const inp = document.getElementById('tasks')
+//create cached element reference for button
+const btn = document.getElementById('submit-button')
+//create cached element reference for ul
+const uL = document.getElementById('todo-list')
+

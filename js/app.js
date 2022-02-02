3692afa51c9cfab56ea0b4652804b19d84811862
AndrewG1993 <andrew.ryan.gallop@gmail.com>
2022-02-02 17:24:45 -0600

    addEventListener

--- js/app.js
+++ js/app.js
@@ -5,3 +5,17 @@ const btn = document.getElementById('submit-button')
 //create cached element reference for ul
 const uL = document.getElementById('todo-list')
 
+
+//addEventListener to btn
+btn.addEventListener('click', function(evt) {
+  //create new li element
+  const newLi = document.createElement('li')
+  //set text of newLi to text of inp
+  newLi.innerText = inp.value 
+  //reset inp text field to empty string
+  inp.value=""
+  //add newLi to the page inside ul
+  uL.appendChild(newLi)
+
+  console.log(newLi)
+})
